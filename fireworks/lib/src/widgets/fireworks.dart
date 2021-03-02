06bc3f506612629da0501960168bb7409445a651
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-03-02 21:35:49 +0000

    Add customization options for @nyener (#24)

--- fireworks/lib/src/widgets/fireworks.dart
+++ fireworks/lib/src/widgets/fireworks.dart
@@ -6,30 +6,21 @@ class Fireworks extends LeafRenderObjectWidget {
   Fireworks({
     Key? key,
     required this.controller,
-    this.showYear = true,
   }) : super(key: key);
 
   /// The controller that manages the fireworks and tells the render box what
   /// and when to paint.
   final FireworkController controller;
 
-  /// Whether to paint the year text in the foreground.
-  ///
-  /// When `false`, the year text is completely omitted.
-  final bool showYear;
-
   @override
   RenderObject createRenderObject(BuildContext context) {
     return RenderFireworks(
       controller: controller,
-      showYear: showYear,
     );
   }
 
   @override
   void updateRenderObject(BuildContext context, RenderFireworks renderObject) {
-    renderObject
-      ..controller = controller
-      ..showYear = showYear;
+    renderObject.controller = controller;
   }
 }
